bbdca86c4625f098152e2f4b8a36bc8f0fb73a12
Muhammed Savaad <savaad.code@gmail.com>
2025-06-30 15:12:36 +0530

    Basic setup

---
+++ src/assets/assets.ts
@@ -0,0 +1,11 @@
+import logoImage from "./logo.png";
+import heroImage from "./hero.png";
+import registerImage from "./register.png";
+import loginImage from "./login.png";
+
+export const assets = {
+    logoImage,
+    heroImage,
+    registerImage,
+    loginImage
+}
